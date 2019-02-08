45db0fe4cd55086e55fd9fed48dbdcb130ae1c35
deepamlani123 <47462262+deepamlani123@users.noreply.github.com>
2019-02-08 15:51:35 -0500

    Add files via upload

---
+++ DiabetesDataFrame.R
@@ -0,0 +1,17 @@
+# Multi analysis 
+diabetesDataFrame<-read.csv("file:///C:/Users/deepa/Desktop/Assignment 1 (1)/Assignment 1/Deep Assignment/data-01.csv")
+  
+diabetesDataFrame
+
+ls(diabetesDataFrame)
+
+names(diabetesDataFrame)
+
+str(diabetesDataFrame)
+
+
+dim(diabetesDataFrame)
+
+head(diabetesDataFrame)
+
+tail(diabetesDataFrame)
